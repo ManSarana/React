e1d38df68c070fa2cba7c392f790878c6377ea60
sarana <saranasudsang@gmail.com>
2022-04-18 21:49:58 +0700

    180465

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import AppHeader from './components/AppHeader'
+import TattooItem from './components/TattooItem'
+import TattooPost from './components/TattooPost'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div className="app">
+            <AppHeader />
+            <div className="app-grid">
+               <TattooItem title="Germany" thumbnailUrl="/images/germany.png" />
+               <TattooItem title="Thailand" thumbnailUrl="/images/thailand.png" />
+               <TattooItem title="Switzerland" thumbnailUrl="/images/switzerland.png" />
+               <TattooItem title="United kingdom" thumbnailUrl="/images/united-kingdom.png" />
+            </div>
+            {/* <TattooPost /> */}
+        </div>
+    )
+
 }
 
 export default App;
---
+++ src/components/TattooPost.js
@@ -0,0 +1,15 @@
+import './TattooPost.css'
+
+function TattooPost() {
+    return (
+        <div className="tattoo-post">
+            <div className="tatoo-post-bg" />
+            <div className="tatoo-post-content">
+                <img src="/images/germany.png" />
+                <h4>Germany</h4>
+            </div>
+        </div>
+    )
+}
+
+export default TattooPost
