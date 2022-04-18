ad68819e6987077578d75d91b5a19f2e911c9f90
Sarana <72848281+saranasudsangkuy@users.noreply.github.com>
2022-04-18 22:00:46 +0700

    180465

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import './App.css';
-import AppHeader from './components/AppHeader'
-import TattooItem from './components/TattooItem'
-import TattooPost from './components/TattooPost'
+import "./App.css";
+import AppHeader from "./components/AppHeader";
+import TattooItem from "./components/TattooItem";
+// import TattooPost from './components/TattooPost'
 
 function App() {
-    return(
-        <div className="app">
-            <AppHeader />
-            <div className="app-grid">
-               <TattooItem title="Germany" thumbnailUrl="/images/germany.png" />
-               <TattooItem title="Thailand" thumbnailUrl="/images/thailand.png" />
-               <TattooItem title="Switzerland" thumbnailUrl="/images/switzerland.png" />
-               <TattooItem title="United kingdom" thumbnailUrl="/images/united-kingdom.png" />
-            </div>
-            {/* <TattooPost /> */}
-        </div>
-    )
-
+  return (
+    <div className="app">
+      <AppHeader />
+      <div className="app-grid">
+        <TattooItem title="Germany" thumbnailUrl="/images/germany.png" />
+        <TattooItem title="Thailand" thumbnailUrl="/images/thailand.png" />
+        <TattooItem
+          title="Switzerland"
+          thumbnailUrl="/images/switzerland.png"
+        />
+        <TattooItem
+          title="United kingdom"
+          thumbnailUrl="/images/united-kingdom.png"
+        />
+      </div>
+      {/* <TattooPost /> */}
+    </div>
+  );
 }
 
 export default App;
