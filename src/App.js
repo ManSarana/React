179adc6c462277b59cea8d1f897fb1be3b7d0a45
sarana <saranasudsang@gmail.com>
2022-04-19 20:29:58 +0700

    190465

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import "./App.css";
 import AppHeader from "./components/AppHeader";
 import TattooItem from "./components/TattooItem";
 // import TattooPost from './components/TattooPost'
+import tattoos from './data/tattoos'
 
 function App() {
+  const [selectedTattoos, setSelectedTattoos] = useState(null)
+  const tattooElement = tattoos.map((tattoo, index) => {
+    return <TattooItem key={index} tattoo={tattoo}/>;
+  })
   return (
     <div className="app">
       <AppHeader />
       <div className="app-grid">
-        <TattooItem title="Germany" thumbnailUrl="/images/germany.png" />
-        <TattooItem title="Thailand" thumbnailUrl="/images/thailand.png" />
-        <TattooItem
-          title="Switzerland"
-          thumbnailUrl="/images/switzerland.png"
-        />
-        <TattooItem
-          title="United kingdom"
-          thumbnailUrl="/images/united-kingdom.png"
-        />
+        {tattooElement}
       </div>
       {/* <TattooPost /> */}
     </div>
---
+++ src/data/tattoos.js
@@ -0,0 +1,30 @@
+
+const tattoos = [
+    {
+      title: "Germany",
+      thumbnailUrl: "/images/flag1.png"
+    },
+    {
+      title: "Thailand",
+      thumbnailUrl: "/images/flag2.png"
+    },
+    {
+        title: "Switzerland",
+        thumbnailUrl: "/images/flag3.png"
+    },
+    {
+        title: "United-kingdom",
+        thumbnailUrl: "/images/flag4.png"
+    },
+    {
+        title: "Indai",
+        thumbnailUrl: "/images/flag5.png"
+    },
+    {
+        title: "Denmark",
+        thumbnailUrl: "/images/flag6.png"
+    },
+    
+  ]
+  
+export default tattoos;
